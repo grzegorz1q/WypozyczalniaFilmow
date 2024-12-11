9590f7bdf5232233a35e534646c3e47964f3d0fd
grzegorz1q <grzegorbaryla@gmail.com>
2024-12-11 14:57:19 +0100

    RentView

--- WypozyczalniaFilmow/Models/Film.cs
+++ WypozyczalniaFilmow/Models/Film.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
