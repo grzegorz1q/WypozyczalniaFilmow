e14e137702513bfcfa85a4d8b124a1058f854226
grzegorz1q <grzegorbaryla@gmail.com>
2024-12-16 23:13:02 +0100

    baase styles

--- WypozyczalniaFilmow/Database/AppDbContext.cs
+++ WypozyczalniaFilmow/Database/AppDbContext.cs
@@ -68,6 +68,7 @@ namespace WypozyczalniaFilmow.Database
                 .WithMany(r => r.Rents)
                 .HasForeignKey(f => f.FilmId)
                 .OnDelete(DeleteBehavior.Restrict);
+
         }
     }
 }
